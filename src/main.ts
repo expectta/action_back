f7640ef9bde1b6ffa5deaf84400e6cc64f309965
jiwoon <expect.ta@mail.com>
2023-05-02 14:05:13 +0900

    ll

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import serverlessExpress from '@vendia/serverless-express';
+import { Callback, Context, Handler } from 'aws-lambda';
 
 export async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -9,6 +11,20 @@ export async function bootstrap() {
     },
   });
   return app;
+}
+let server: Handler;
+
+export async function handler(
+  event: any,
+  context: Context,
+  callback: Callback,
+) {
+  const app = await bootstrap();
+  await app.init();
+  const expressApp = app.getHttpAdapter().getInstance();
+
+  server = server ?? serverlessExpress({ app: expressApp });
+  return server(event, context, callback);
 }
 
 async function run() {
