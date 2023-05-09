dd8eace331f644e82285fb85e7466f668fd8fe50
jiwoon <expect.ta@mail.com>
2023-05-09 17:10:10 +0900

    ip라이브러리추가

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,14 @@
 import { Controller, Get, Req } from '@nestjs/common';
 import { AppService } from './app.service';
-import { Request } from 'express';
+import { Request } from 'express'; // express의 Request 타입을 임포트
+import { RealIP } from 'nestjs-real-ip';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(@Req() request: Request): string {
-    const ip = request.ip;
+  getHello(@RealIP() ip: string): string {
     return ip;
     // return this.appService.getHello();
   }
